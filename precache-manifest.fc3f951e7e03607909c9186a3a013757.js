2dbe04025d94331721a5ef39368162ca0447361f
Roha SJ Lee <rohasjlee@Rohas-MacBook-Pro.local>
2020-09-16 20:43:02 +0800

    Updates

---
+++ precache-manifest.fc3f951e7e03607909c9186a3a013757.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70d56e32b873b78a8231a67ef1023820",
+    "url": "/index.html"
+  },
+  {
+    "revision": "83b1764b1f4151a9e7a8",
+    "url": "/static/js/2.38abe596.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.38abe596.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cdb1525dcc02a25e0774",
+    "url": "/static/js/main.4ba958fc.chunk.js"
+  },
+  {
+    "revision": "b1820bfc6ade90caaf5b",
+    "url": "/static/js/runtime-main.51c4ed28.js"
+  }
+]);
